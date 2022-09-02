8db5e96fd7c002424bf80c9d4f2d57bdbbe6995b
Ivan <ivangujik@yandex.ru>
2022-09-02 14:01:43 +1000

    added glad

--- RaftEngineCore/src/RaftEngineCore/Application.cpp
+++ RaftEngineCore/src/RaftEngineCore/Application.cpp
@@ -1,5 +1,6 @@
 #include "RaftEngineCore/Application.hpp"
 #include "RaftEngineCore/Log.hpp"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -40,11 +41,19 @@ namespace RaftEngine {
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            LOG_CRITICAL("Failed to initialize GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
